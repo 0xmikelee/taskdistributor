cb267673497417cafaf45e76ee30402bb0c7fa44
Mike Lee <lee.mikemc@gmail.com>
2018-05-09 23:52:40 +0800

    ADD: assign tool value to transcriber tasks, updates transcriber task status

--- Code.js
+++ Code.js
@@ -69,7 +69,7 @@ function getHeaderIndex(sheetName) {
 	if (sheetName === 'Transcribers') {
 		headers = ['Name', 'Email', 'FileID']
 	} else if (sheetName === 'TasksMaster') {
-		headers = ['File', 'MP3 Source', 'Original Source', 'Audio Length', 'Speakers', 'Transcriber 1', 'Transcriber 1 Distribution Date', 'Transcriber 1 Result', 'Transcriber 1 Time Taken', 'Transcriber 1 Tool', 'Transcriber 1 Issues/Bugs', 'Transcriber 2', 'Transcriber 2 Distribution Date', 'Transcriber 2 Result', 'Transcriber 2 Time Taken', 'Transcriber 2 Tool', 'Transcriber 2 Issues/Bugs']
+		headers = ['File', 'MP3 Source', 'Original Source', 'Audio Length', 'Speakers', 'Transcriber 1', 'Transcriber 1 Status', 'Transcriber 1 Distribution Date', 'Transcriber 1 Result', 'Transcriber 1 Time Taken', 'Transcriber 1 Tool', 'Transcriber 1 Issues/Bugs', 'Transcriber 1 Notes', 'Transcriber 2', 'Transcriber 2 Status','Transcriber 2 Distribution Date', 'Transcriber 2 Result', 'Transcriber 2 Time Taken', 'Transcriber 2 Tool', 'Transcriber 2 Issues/Bugs','Transcriber 2 Notes']
 	}
 
 	var _range = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()
@@ -131,6 +131,8 @@ function distributeTasks() {
 	var dataOriginal = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Original Source'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
 	var dataSpeakers = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Speakers'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
 	var dataAudioLength = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Audio Length'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
+	var dataToolTx1 = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Transcriber 1 Tool'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
+	var dataToolTx2 = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Transcriber 2 Tool'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
 
 	var tx1 = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Transcriber 1'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
 	var distributionDatesTx1 = tasksMasterSheet.getRange(2, headerIndexTasksMaster['Transcriber 1 Distribution Date'], tasksMasterSheet.getLastRow() - 1, 1).getValues()
@@ -155,28 +157,28 @@ function distributeTasks() {
 		var headerIndexTxSheet = getHeaderIndexTx(thisTxFileId)
 		var newRowIndex = thisTxSheet.getLastRow() + 1
 
-		Logger.log(requiredDistributionTx1[i])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['File'], 1, 1).setValue(dataFile[requiredDistributionTx1[i].rowArrayIndex])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['MP3 Source'], 1, 1).setValue(dataMP3[requiredDistributionTx1[i].rowArrayIndex])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Original Source'], 1, 1).setValue(dataOriginal[requiredDistributionTx1[i].rowArrayIndex])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Speakers'], 1, 1).setValue(dataSpeakers[requiredDistributionTx1[i].rowArrayIndex])
+		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Tool'], 1, 1).setValue(dataToolTx1[requiredDistributionTx1[i].rowArrayIndex])
 
 		//Use ImportRange formula to get updates from transcriber's tasks list
 		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Result'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Result']+ '))')
 		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Time Taken'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Time Taken']+ '))')
-		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Tool'], 1, 1)
-			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Tool']+ '))')
 		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Issues/Bugs'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Issues/Bugs']+ '))')
+		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Notes'], 1, 1)
+			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Notes']+ '))')
 
 		//Set Distribution Date
 		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Distribution Date'], 1, 1).setValue(new Date())
+		tasksMasterSheet.getRange(requiredDistributionTx1[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 1 Status'], 1, 1).setValue('In Progress')
 
 	}
 
-	Logger.log(requiredDistributionTx2)
 	for (var i = requiredDistributionTx2.length - 1; i >= 0 ; i--) {
 		var thisTxName = requiredDistributionTx2[i].name
 		var thisTxFileId = _.find(txLibrary, function (o) { return o.name === thisTxName }).fileId
@@ -188,19 +190,21 @@ function distributeTasks() {
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['MP3 Source'], 1, 1).setValue(dataMP3[requiredDistributionTx2[i].rowArrayIndex])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Original Source'], 1, 1).setValue(dataOriginal[requiredDistributionTx2[i].rowArrayIndex])
 		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Speakers'], 1, 1).setValue(dataSpeakers[requiredDistributionTx2[i].rowArrayIndex])
+		thisTxSheet.getRange(newRowIndex, headerIndexTxSheet['Tool'], 1, 1).setValue(dataToolTx2[requiredDistributionTx2[i].rowArrayIndex])
 
 		//Use ImportRange formula to get updates from transcriber's tasks list
 		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Result'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Result']+ '))')
 		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Time Taken'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Time Taken']+ '))')
-		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Tool'], 1, 1)
-			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Tool']+ '))')
 		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Issues/Bugs'], 1, 1)
 			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Issues/Bugs']+ '))')
+		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Notes'], 1, 1)
+			.setFormula('=ImportRange("https://docs.google.com/spreadsheets/d/' + thisTxFileId + '", "Tasks!" & address(' + newRowIndex + ',' + headerIndexTxSheet['Notes']+ '))')
 
 		//Set Distribution Date
 		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Distribution Date'], 1, 1).setValue(new Date())
+		tasksMasterSheet.getRange(requiredDistributionTx2[i].rowArrayIndex + 2, headerIndexTasksMaster['Transcriber 2 Status'], 1, 1).setValue('In Progress')
 	}
 
 	SpreadsheetApp.getActiveSpreadsheet().toast('Tasks Assignment Completed', '', 3)
